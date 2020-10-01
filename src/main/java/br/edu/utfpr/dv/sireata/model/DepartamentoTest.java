aff074dbc45067777b0252ce36d409081d837c94
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-10-01 15:45:32 -0300

    atualização teste junit

--- src/main/java/br/edu/utfpr/dv/sireata/model/DepartamentoTest.java
+++ src/main/java/br/edu/utfpr/dv/sireata/model/DepartamentoTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DepartamentoTest {
 
     @Test
-    void TestDepartment() {
+    void TestDepartmentName() {
         Departamento d = new Departamento();
 
         d.setNome("Teste");
@@ -15,6 +15,23 @@ class DepartamentoTest {
 
         assertEquals("Teste", name);
     }
+    @Test
+    void TestDepartmentSite() {
+        Departamento d = new Departamento();
 
+        d.setNome("www.teste.com.br");
+        String name = d.getNome();
+
+        assertEquals("www.teste.com.br", name);
+    }
+    @Test
+    void TestDepartmentId() {
+        Departamento d = new Departamento();
+
+        d.setIdDepartamento(1);
+        int id = d.getIdDepartamento();
+
+        assertEquals(1, id);
+    }
 
 }
